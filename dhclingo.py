96e0ddeda70aa9e6a5e1ed13705ac30fc7476837
Paul Ogris <paul@tsahyt.com>
2018-07-25 13:51:23 +0200

    Add impossible set

--- dhclingo.py
+++ dhclingo.py
@@ -18,21 +18,28 @@ class Declarative(object):
         self.solver.add("base", [], instance_program)
 
         self.__external_sigs = []
+        self.__result_sigs = []
         with self.solver.builder() as b:
             clingo.parse_program(heuristic_program, 
-                    lambda a: self.__collect_externals(b, a))
+                    lambda a: self.__collect_watches(b, a))
         self.solver.ground([("base",[])])
 
         # define mappings
         self.__externals = dict()
         self.__ext_lits = dict()
         self.__lit_exts = dict()
+        self.__lit_ress = dict()
+        self.__impossible = set()
 
-    def __collect_externals(self, builder, a):
+    def __collect_watches(self, builder, a):
         if a.type == clingo.ast.ASTType.External:
             name = a.atom.term.name
             arity = len(a.atom.term.arguments)
             self.__external_sigs.append((name, arity))
+        elif a.type == clingo.ast.ASTType.Rule and a.head.atom.term.name == "heuristic":
+            name = a.head.atom.term.arguments[0].name
+            arity = len(a.head.atom.term.arguments[0].arguments)
+            self.__result_sigs.append((name,arity))
         builder.add(a)
     
     def decide(self,vsids):
@@ -55,17 +62,18 @@ class Declarative(object):
 
         :type model: clingo.Model
         """
-        hlog.debug("model: {}".format(model))
-        syms = (x for x in model.symbols(atoms=True) if x.name == "heuristic")
+        # hlog.debug("model: {}".format(model))
+        syms = (x for x in model.symbols(atoms=True) if x.name == "heuristic"
+                and str(x.arguments[0]) not in self.__impossible)
         return syms.next()
 
     def init(self, init):
         for a in init.symbolic_atoms:
             name = a.symbol.name
             alen = len(a.symbol.arguments)
+            lit = init.solver_literal(a.literal)
             if (name, alen) in self.__external_sigs:
                 # watch in main
-                lit = init.solver_literal(a.literal)
                 init.add_watch(lit)
                 init.add_watch(-lit)
 
@@ -78,6 +86,16 @@ class Declarative(object):
                     self.__lit_exts[alit].add(f)
                 except KeyError:
                     self.__lit_exts[alit] = set([f])
+            if (name, alen) in self.__result_sigs:
+                init.add_watch(lit)
+                init.add_watch(-lit)
+
+                alit = abs(lit)
+                f = str(clingo.Function(name, a.symbol.arguments))
+                try:
+                    self.__lit_ress[alit].add(f)
+                except KeyError:
+                    self.__lit_ress[alit] = set([f])
 
         hlog.debug("ext_lits: {}".format(self.__ext_lits))
 
@@ -86,9 +104,19 @@ class Declarative(object):
         for l in changes:
             for e in self.__lit_exts[abs(l)]:
                 self.__externals[e] = ctl.assignment.is_true(abs(l))
+            try:
+                for a in self.__lit_ress[abs(l)]:
+                    self.__impossible.add(a)
+            except KeyError:
+                pass
 
     def undo(self, thread_id, assign, changes):
         hlog.debug("undo {}".format(changes))
         for l in changes:
             for e in self.__lit_exts[abs(l)]:
                 self.__externals[e] = assign.is_true(abs(l))
+            try:
+                for a in self.__lit_ress[abs(l)]:
+                    self.__impossible.remove(a)
+            except KeyError:
+                pass
